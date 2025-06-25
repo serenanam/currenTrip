305ff600678a227305e7c4b366e14d7a573392ff
serenanam <73319962+serenanam@users.noreply.github.com>
2025-06-25 20:19:49 +0900

    update homepage blurb

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <>
     <div className="homepage-text-container">
-      <p> Welcome to CurrenTrip — your all-in-one travel budget tracker.
+      <p> Welcome to CurrenTrip — your all-in-one travel budget tracker that helps you effortlessly track and manage your travel expenses across multiple currencies.
         Budget smarter, spend better, and keep track of every expense across currencies while you explore the world.
-      Stay on top of your budget with easy-to-use tools, clear visuals, and real-time currency conversions — so you can focus on the journey, not the math.
+      Stay on top of your budget with easy-to-use tools, clear visuals, and real-time currency conversions — so you can focus on your <b>current trip</b>, not the math.
       </p>
     </div>
     </>
